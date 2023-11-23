7259d295dd379acc26e04852db2f524a23356cb5
Dakkaoui050 <80848743+Dakkaoui050@users.noreply.github.com>
2023-11-23 14:36:39 +0100

    test

--- zombie survior/Assets/Kornee/Shop/Scrips/Shop Manager.cs
+++ zombie survior/Assets/Kornee/Shop/Scrips/Shop Manager.cs
@@ -13,6 +13,7 @@ public class ShopManager : MonoBehaviour
     [SerializeField] private GameObject[] Press;
     [SerializeField] private GameObject Shop;
     public bool UseShop;
+    public Collider2D collision1;
     
     // Start is called before the first frame update
     public void Awake()
@@ -26,6 +27,39 @@ public class ShopManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (collision1 != null)
+        {
+
+            if (collision1.gameObject.tag == "Player")
+            {
+                Player = collision1.gameObject.GetComponent<player>();
+                if (Player.playerIndex == 0)
+                {
+                    if (Input.GetButtonDown("Action 1") && !UseShop)
+                    {
+                        Shop.GetComponent<Canvas>().enabled = true;
+                        Shop.GetComponent<ShopInteractions>().play = 0;
+                        Press[0].SetActive(true);
+                        UseShop = true;
+                        Time.timeScale = 0;
+                    }
+
+                }
+                else if (Player.playerIndex == 1)
+                {
+                    if (Input.GetButtonDown("Fire1") && !UseShop)
+                    {
+                        Shop.GetComponent<Canvas>().enabled = true;
+                        Shop.GetComponent<ShopInteractions>().play = 1;
+                        Press[0].SetActive(true);
+                        UseShop = true;
+                        Time.timeScale = 0;
+                    }
+
+                }
+            }
+        }
+
         if (Player.playerIndex == 0)
         {
             if (Input.GetButton("Action 6") && UseShop)
@@ -43,6 +77,10 @@ public class ShopManager : MonoBehaviour
         }
         
     }
+    private void FixedUpdate()
+    {
+        
+    }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
@@ -52,6 +90,7 @@ public class ShopManager : MonoBehaviour
             foreach (GameObject item in Press)
             {
                 item.SetActive(true);
+                collision1 = other;
             }
             
         }
@@ -64,46 +103,11 @@ public class ShopManager : MonoBehaviour
             foreach (GameObject item in Press)
             {
                 item.SetActive(false);
+                collision1 = null;
             }
         }
     }
-    private void OnTriggerStay2D(Collider2D collision)
-    {
-        if (collision.tag == "Player")
-        {
-            Player = collision.gameObject.GetComponent<player>();
-            foreach (GameObject item in Press)
-            {
-                item.SetActive(true);
-            }
-            if (Player.playerIndex == 0)
-            {
-                if (Input.GetButtonDown("Action 1") && !UseShop)
-                {
-                    print("test");
-                    Shop.GetComponent<Canvas>().enabled = true;
-                    Shop.GetComponent<ShopInteractions>().play = 0;
-                    Press[0].SetActive(true);
-                    UseShop = true;
-                    Time.timeScale = 0;
-                }
 
-            }
-            else if (Player.playerIndex == 1)
-            {
-                if (Input.GetButtonDown("Fire1") && !UseShop)
-                {
-                    print("test");
-                    Shop.GetComponent<Canvas>().enabled = true;
-                    Shop.GetComponent<ShopInteractions>().play = 1;
-                    Press[0].SetActive(true);
-                    UseShop = true;
-                    Time.timeScale = 0;
-                }
-
-            }
-        }
-    }
    
 
     public void closeShop()
